a5eb97de553a13a0badc543177e1a53105f6d3e6
Miles Weber <milesweber@comcast.net>
2022-10-11 11:29:29 -0400

    first commit

---
+++ console_equals.js
@@ -0,0 +1,7 @@
+/**
+ * * Compares two objects using ===
+ * @param obj1 first object to be comapred
+ * @param obj2 second object to be compared
+ * @return returns true if obj1 === obj2  
+ */
+console.equals = (obj1, obj2) => { return obj1 === obj2}
